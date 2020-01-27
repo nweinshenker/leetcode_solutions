a33bfd6039c31527d4ddc518e061e5e76ed0c207
nweinshenker <nweinshenker@wisc.edu>
2020-01-27 01:24:51 -0600

    Two sum problem with sorted input. Time complexity is O(n) array with a binary search

---
+++ January/twoSumSorted.java
@@ -0,0 +1,21 @@
+class twoSumSorted {
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers.length < 2 || numbers == null) {
+            return null;
+        }
+
+        int i = 0;
+        int j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum > target) {
+                j--;
+            } else if (sum < target) {
+                i++;
+            } else {
+                break;
+            }
+        }
+        return new int[] { i + 1, j + 1 };
+    }
+}
