46c9055476dbc8397cf674b56dc9688f4cf8237a
nweinshenker <nweinshenker@wisc.edu>
2020-05-20 10:07:23 -0500

    Converts sorted array to balanced binary search tree

---
+++ May/Convert_Sorted_Array_to_Binary_Search_Tree.java
@@ -0,0 +1,34 @@
+package May;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) {this.val = val;}
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Convert_Sorted_Array_to_Binary_Search_Tree {
+    public TreeNode solution(int[] nums) {
+        if (nums == null) {
+            return null;
+        }
+        return balanced(nums, 0, nums.length);
+    }
+
+    public TreeNode balanced(int [] nums, int startIdx, int endIdx) {
+        if (startIdx > endIdx) {
+            return null;
+        }
+
+        int mid = (startIdx + endIdx) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = balanced(nums, startIdx, mid - 1);
+        root.right = balanced(nums, mid + 1, endIdx);
+        return root;
+    }
+}
