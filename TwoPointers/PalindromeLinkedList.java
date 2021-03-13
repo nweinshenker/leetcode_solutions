6a679e311e34102039603abfdfa78a0275277a91
nweinshenker <nweinshenker@wisc.edu>
2021-03-12 19:25:21 -0600

    Two pointer solution

---
+++ TwoPointers/PalindromeLinkedList.java
@@ -0,0 +1,37 @@
+package TwoPointers;
+
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {};
+    ListNode(int val) { this.val = val;}
+}
+
+class TwoPointers{
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<>();
+
+        // Convert LinkedList into ArrayList.
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        // Use two-pointer technique to check for palindrome.
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            // Note that we must use ! .equals instead of !=
+            // because we are comparing Integer, not int.
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
